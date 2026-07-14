8b08ab6a8b693825c381397ab444c23dd432e664
Rens <rens@domeinstam.nl>
2026-07-14 07:41:57 +0200

    deploy update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "relax-breathing-humming-v151";
+const CACHE = "relax-breathing-humming-v152";
 
 self.addEventListener("install", e => {
   // Niet automatisch skipWaiting: de nieuwe versie wacht tot de gebruiker
